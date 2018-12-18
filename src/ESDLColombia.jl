d285f8f5685868ce0e83f15120126af806df54d0
meggart <fabiangans@gmx.de>
2018-12-18 11:01:46 +0100

    first commit

--- src/ESDLColombia.jl
+++ src/ESDLColombia.jl
@@ -1,5 +1,22 @@
 module ESDLColombia
+import ESDL
 
-# package code goes here
+colombia_staticvars = ["Bioma_IAvH2",
+ "CKCM_hidrosig",
+ "CKC_hidrosig",
+ "CKe_hidrosig",
+ "KDE_hidrosig",
+ "bio_units",
+ "canopy_height",
+ "municipios"]
+foreach(i->push!(ESDL.CubeAPI.static_vars,i),colombia_staticvars)
+
+ESDL.CubeAPI.known_names["Bioma_IAvH2"] = "Biome"
+ESDL.CubeAPI.known_names["municipios"]  = "Municipio"
+
+biomedict = include("Bioma_IAvH2_ID.jl")
+municipiosdict = include("municipios_ID.jl")
+ESDL.CubeAPI.known_labels["Bioma_IAvH2"] = biomedict
+ESDL.CubeAPI.known_labels["municipios"]  = municipiosdict
 
 end # module
